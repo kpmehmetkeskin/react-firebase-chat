1047fc4a0890e59d543f15e9d5e00712041fe8eb
kpmehmetkeskin <cs.mehmetkeskin@gmail.com>
2022-07-04 23:40:44 +0300

    Firebase integrated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
   // your config
+
+    apiKey: "AIzaSyAlwgnETafa0JFgAWYfbPnEe1BycLlF5a8",
+    authDomain: "wmapchat.firebaseapp.com",
+    projectId: "wmapchat",
+    storageBucket: "wmapchat.appspot.com",
+    messagingSenderId: "623997961309",
+    appId: "1:623997961309:web:92de103b097aa4c36e39b4",
+    measurementId: "G-0SY71RB1Q2"
+  
 })
 
 const auth = firebase.auth();
